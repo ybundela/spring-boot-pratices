ac9b30c43d27396b60b99db95bc4a858ac357659
Yogendra Singh Bundela <135322047+ybundela@users.noreply.github.com>
2023-09-23 22:34:23 +0100

    rest controller

---
+++ 02-restapi-crud/src/main/java/com/restServices/apisDemo/ApisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.restServices.apisDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisDemoApplication.class, args);
+	}
+
+}
---
+++ 02-restapi-crud/src/test/java/com/restServices/apisDemo/ApisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restServices.apisDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
